fc7fc925c2aaa673bb9aa7b98aa7fff8ab41425c
David Shi <mtsocom@hotmail.com>
2018-05-27 21:57:59 +0800

    test extend

--- src/three/tetrisApp.js
+++ src/three/tetrisApp.js
@@ -1,4 +1,5 @@
 import PlayGround from './app/playground';
+import { extendPaths, nearlyEqual } from './utils';
 
 export default class TetrisApp {
   constructor() {
@@ -9,6 +10,8 @@ export default class TetrisApp {
     this.mOptions = {
       domElement: document.querySelector('#WebGL-output'),
     };
+
+    this.testPathFunc();
   }
 
   init(options) {
@@ -24,5 +27,109 @@ export default class TetrisApp {
     const x = ( event.clientX / this.mOptions.domElement.clientWidth ) * 2 - 1;
     const y = - ( event.clientY / this.mOptions.domElement.clientHeight ) * 2 + 1;
     this.mPlayGround.addCubeByMousePosition({ x, y });
+  }
+
+  testPathFunc() {
+    const mycanvas = document.createElement('canvas');
+    mycanvas.id = 'mycanvas';
+    document.body.appendChild(mycanvas);
+    mycanvas.width = 1280;
+    mycanvas.height = 960;
+    mycanvas.style.position = 'absolute';
+    mycanvas.style.left = 0;
+    mycanvas.style.top = 0;
+    mycanvas.style.width = '1280px';
+    mycanvas.style.height = '960px';
+    mycanvas.style.background = 'aliceblue';
+    mycanvas.style.zIndex = 1000;
+    document.body.appendChild(mycanvas);
+
+    const context = mycanvas.getContext('2d');
+
+    const pathsArray = [
+      [
+        { x: 200, y: 200, },
+        { x: 300, y: 200, },
+        { x: 300, y: 300, },
+        { x: 200, y: 300, },
+        { x: 200, y: 200, },
+      ],
+      [
+        { x: 421, y: 232, },
+        { x: 641, y: 232, },
+      ],
+      [
+        { x: 400, y: 300, },
+        { x: 400, y: 500, },
+        { x: 500, y: 500, },
+        { x: 500, y: 600, },
+        { x: 700, y: 600, },
+        { x: 700, y: 350, },
+        { x: 650, y: 350, },
+        { x: 650, y: 300, },
+      ],
+      [
+        { x: 400, y: 700, },
+        { x: 400, y: 900, },
+        { x: 50, y: 900, },
+        { x: 50, y: 750, },
+        { x: 100, y: 750, },
+        { x: 100, y: 700, },
+      ],
+      [
+        { x: 900, y: 700, },
+        { x: 900, y: 900, },
+        { x: 750, y: 900, },
+        { x: 650, y: 800, },
+        { x: 650, y: 700, },
+      ],
+      [
+        { x: 600, y: 500, },
+        { x: 661, y: 666, },
+        { x: 461, y: 701, },
+        { x: 402, y: 843, },
+        { x: 200, y: 643, },
+        { x: 200, y: 500, },
+      ],
+    ];
+
+    pathsArray.forEach(paths => {
+      this.drawPath(paths, context);
+    });
+
+    context.strokeStyle = 'blue';
+
+    pathsArray.forEach(paths => {
+      let newPtahs = extendPaths(paths, 20);
+      this.drawPath(newPtahs, context);
+    });
+  }
+
+  drawPath(paths, context) {
+    if (paths.length < 2) {
+      return;
+    }
+    let isClose = false;
+    if (nearlyEqual(paths[0].x, paths[paths.length - 1]) && nearlyEqual(paths[0].x, paths[paths.length - 1])) {
+      isClose = true;
+    }
+
+    context.beginPath();
+
+    let start = false;
+    paths.forEach(p => {
+      if (!start) {
+        context.moveTo(p.x, p.y);
+        start = true;
+      } else {
+        context.lineTo(p.x, p.y);
+      }
+    });
+    context.stroke();
+
+    if (isClose) {
+      context.closePath();
+    }
+
   }
 }
--- src/three/utils.js
+++ src/three/utils.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+const PI = 3.1415926;
 export function getRandomColor() {
   const letters = '0123456789ABCDEF';
   const color = Array(3);
@@ -5,4 +7,93 @@ export function getRandomColor() {
     color[i] = letters[Math.floor(Math.random() * 16)];
   }
   return `#${color[0]}${color[0]}${color[1]}${color[1]}${color[2]}${color[2]}`;
+}
+
+export function nearlyEqual(x, y) {
+  return Math.abs(x - y) < 0.001;
+}
+
+export function extendPaths(paths, extendWidth, keepRightAngle = true) {
+  let loopPaths = [];
+  let isClosePath = (nearlyEqual(paths[0].x, paths[paths.length - 1].x) && nearlyEqual(paths[0].x, paths[paths.length - 1].y));
+  let prevVector = new THREE.Vector2();
+
+  if (paths.length <= 2) {
+    // build a parallel line and close it...
+    return paths;
+  }
+
+  // direction of start point to end point
+  let direction = new THREE.Vector2(
+    paths[paths.length - 1].x - paths[0].x,
+    paths[paths.length - 1].y - paths[0].y);
+
+  let rotation = -PI / 2;
+  let directionAdjusted = false;
+
+  for (let i = 0; i < paths.length; i++) {
+    let p0 = paths[i];
+    let p1;
+
+    // for closed path, need check the last point carefully
+    if (i === paths.length - 1) {
+      if (isClosePath) {
+        p1 = paths[1];
+      } else {
+        break;
+      }
+    } else {
+      p1 = paths[i + 1];
+    }
+
+    // a extend direction vector
+    let extendVector = new THREE.Vector2(p1.x - p0.x, p1.y - p0.y);
+    extendVector.normalize().multiplyScalar(extendWidth).rotateAround(new THREE.Vector2(0, 0), rotation);
+
+    // reverse extend direction if needed
+    if (!directionAdjusted) {
+      if (direction.dot(extendVector) > 0) {
+        rotation = PI / 2;
+        extendVector.negate();
+      }
+      directionAdjusted = true;
+    }
+
+    // the start point of this edge
+    let v0 = new THREE.Vector2();
+    v0.copy(extendVector).add(new THREE.Vector2(p0.x, p0.y));
+
+    let dot = extendVector.x * prevVector.x + extendVector.y * prevVector.y;
+    let det = extendVector.x * prevVector.y - extendVector.y * prevVector.x;
+    let angle = Math.atan2(det, dot);
+    console.log(`${i}: angle: ${angle * 180 / PI} rot: ${rotation * 180 /PI}`);
+
+    // if need handle right angle...
+    //if (keepRightAngle && loopPaths.length > 0 && nearlyEqual(extendVector.dot(prevVector), 0)) {
+    if (keepRightAngle && loopPaths.length > 0 && nearlyEqual(extendVector.dot(prevVector), 0)) {
+      // check if right angle...
+      // let dot = extendVector.x * prevVector.x + extendVector.y * prevVector.y;
+      // let det = extendVector.x * prevVector.y - extendVector.y * prevVector.x;
+      // let angle = Math.atan2(det, dot);
+      loopPaths.pop();
+      v0 = (extendVector.clone().add(prevVector).add(new THREE.Vector2(p0.x, p0.y)));
+    }
+    loopPaths.push(v0);
+
+    // handle last point of closed path
+    if (isClosePath && (i === paths.length - 1)) {
+      loopPaths.shift();
+      loopPaths.unshift(v0);
+      break;
+    }
+
+    // the end point of this edge...
+    let v1 = new THREE.Vector2();
+    v1.copy(extendVector).add(new THREE.Vector2(p1.x, p1.y));
+    loopPaths.push(v1);
+
+    prevVector.copy(extendVector);
+  }
+
+  return loopPaths;
 }
